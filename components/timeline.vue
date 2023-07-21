d6743f2576a4794634510ea7a5ba59c17818d43e
Yvonne Becker <73485093+yvnnbcker@users.noreply.github.com>
2023-07-21 21:49:14 +0200

    timeline responsive desktop

--- components/timeline.vue
+++ components/timeline.vue
@@ -1,45 +1,44 @@
 <template>
     <div class="timeline">
-        <div class="scribble scribble4"><img src="/scribble-dot3.svg"></div>
-        <div class="timeline-content">
-            <div class="first-column">
-                <TimelineDot
-                class="dot1"
-                img="/magazin-yolo.jpg"
-                alt=""
-                year="2023"
-                title="änny viral"
-                desc="Vision Quote: Sed mi ex, luctus sed sem vel, cursus pretium lorem. Phasellus "
-                bg="red"
-                order="timeline-img-first"/>
-                <TimelineDot
-                class="dot2"
-                img="/testing.png"
-                alt=""
-                year="2021"
-                title="Testings"
-                desc="Vision Quote: Sed mi ex, luctus sed sem vel, cursus pretium lorem. Phasellus "
-                bg="mint"
-                order="timeline-img-first"/>
-            </div>
-            <div class="second-column">
-                <TimelineDot
-                class="dot3"
-                img="/produktion.jpeg"
-                alt=""
-                year="2022" 
-                title="Produktions-firmen"
-                desc="Vision Quote: Sed mi ex, luctus sed sem vel, cursus pretium lorem. Phasellus "
-                bg="babyblue"/>
-                <TimelineDot
-                class="dot4"
-                img="/geburtsstunde.jpg"
-                alt=""
-                year="2020"
-                title="Geburtsstunde"
-                desc="Vision Quote: Sed mi ex, luctus sed sem vel, cursus pretium lorem. Phasellus"
-                bg="rose"/>
-            </div>
+        <div class="timeline-bg">
+            <img src="/line.svg">
         </div>
+        <div class="scribble scribble4">
+            <img src="/scribble-dot3.svg">
+        </div>
+            <TimelineDot
+            class="dot1"
+            img="/magazin-yolo.jpg"
+            alt=""
+            year="2023"
+            title="änny viral"
+            desc="Vision Quote: Sed mi ex, luctus sed sem vel, cursus pretium lorem. Phasellus "
+            bg="red"
+            order="timeline-img-first"/>
+            <TimelineDot
+            class="dot2"
+            img="/produktion.jpeg"
+            alt=""
+            year="2022" 
+            title="Produktions-firmen"
+            desc="Vision Quote: Sed mi ex, luctus sed sem vel, cursus pretium lorem. Phasellus "
+            bg="babyblue"/>
+            <TimelineDot
+            class="dot3"
+            img="/testing.png"
+            alt=""
+            year="2021"
+            title="Testings"
+            desc="Vision Quote: Sed mi ex, luctus sed sem vel, cursus pretium lorem. Phasellus "
+            bg="mint"
+            order="timeline-img-first"/>
+            <TimelineDot
+            class="dot4"
+            img="/geburtsstunde.jpg"
+            alt=""
+            year="2020"
+            title="Geburtsstunde"
+            desc="Vision Quote: Sed mi ex, luctus sed sem vel, cursus pretium lorem. Phasellus"
+            bg="rose"/>
     </div>
 </template>
