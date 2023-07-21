a65ecfa4573a07b4851492fbf3fbb8d118adf09c
Yvonne Becker <73485093+yvnnbcker@users.noreply.github.com>
2023-07-22 01:17:40 +0200

    ds & i done

--- pages/impressum.vue
+++ pages/impressum.vue
@@ -1,50 +1,38 @@
 <template>
     <div class="legal">
-      <div class="legal-content">
-        <div class="privacy-policy">
-            <h1>Privacy Policy</h1>
-            <br ><br ><h2>Preamble</h2>
-            <br >Article 12 of the EU General Data Protection Regulation (DSGVO for short) requires that I explain to you "in a precise, transparent, understandable and easily accessible form [and] in clear and simple language" what is happening here on this website.
-            <br ><br ><br >
-            
-            <h2>1. Hosting</h2>
-            <br >This website is hosted by the Netlify. The hosting services are used to provide the following services: Infrastructure and platform services, computing capacity, storage space and database services, security services and technical maintenance services.
-            <br >
-            In this context the hosting providers, process inventory data, contact data, content data, contract data, usage data, meta data and communication data of customers, interested parties and visitors of this online offer on the basis of our legitimate interests in an efficient and secure provision of this online offer pursuant to Art. 6 para. 1 lit. f DSGVO in conjunction with Art. 28 DSGVO. Art. 28 DSGVO (conclusion of order processing contract).
-            <br ><br ><br >
-            
-            <h2>2. SSL or TLS encryption</h2>
-            <br >For security reasons and to protect the transmission of confidential content that you send to me as the site operator, our website uses SSL or TLS encryption. This means that data you transmit via this website cannot be read by third parties. You can recognize an encrypted connection by the "https://" address line of your browser and the lock symbol in the browser line.
-            <br ><br ><br >
-            
-            <h2>3. Contact</h2>
-            <br >If you send me an e-mail, you have to live with the fact that I receive data from you. I will then see your e-mail address, possibly also your IP address and if I make a real effort and manually evaluate the X header of your e-mail, I may even be able to see the name of the computer from which you wrote the message. This is not magic or hacker art, but an Internet standard. Anyone who can read it can access this data. This is technical - and has been the case for many years. If you send me your data without being asked, you may assume that I will protect your e-mail just as well or badly as all my other e-mails. If you should ever think that I should delete the e-mail you sent me without being asked, you may politely ask me to do so - but I won't promise anything. Again, if you can't live with that, please don't send me an email.
-            <br ><br ><br >
-            
-            <h2>4. Tools</h2>
-            <br ><b>4.1 Adobe Typekit Web Fonts</b>
-            <br > This website uses Adobe Typekit Web Fonts of the responsible party Adobe Systems Software Ireland Limited, 4-6 Riverwalk, Citywest Business Campus, Dublin 24, Republic of Ireland hereinafter referred to as "Adobe" for the uniform display of fonts. 
-                    With the certification according to the EU-US Privacy Shield <a href="https://www.privacyshield.gov/participant?id=a2zt0000000TNo9AAG">https://www.privacyshield.gov/participant?id=a2zt0000000TNo9AAG</a>, Adobe guarantees that the data protection requirements of the EU are also complied with when processing data in the USA. When you call up a page, your browser loads the required web fonts into your browser cache in order to display texts and fonts correctly. 
-                    For this purpose, the browser you use must connect to Adobe's servers. This gives Adobe knowledge that our website has been accessed via your IP address. According to the DSGVO, the IP address is a personal data and can be transmitted to a server of Adobe in the USA and stored there. The person responsible for this website has no influence on this data transmission.
-                    The legal basis for the use of Adobe Typekit Web Fonts is legitimate interest within the meaning of Art. 6 (1) f DSGVO and enables an appealing and consistent presentation of the website.
-                    <br >If your browser does not support web fonts, a standard font from your computer will be used.
-                    <br >For more information on Adobe Typekit Web Fonts, please visit <a href="https://typekit.com">https://typekit.com</a> and see Adobe's privacy policy: <a href="https://www.adobe.com/privacy/policy.html">https://www.adobe.com/privacy/policy.html</a>.
-            <br ><br ><br ><br ><br ><br >
+        <div class="legal-content">
+            <div class="legal-representator">
+                <h1>Legal Notice (Impressum)</h1>
+                <br >
+                    Alina Gaidies
+                <br > Kapuzinergasse 3
+                <br > 73525 Schwäbisch Gmünd
+                <br > Germany
+                <br ><br >
+                <b>Represented by:</b>
+                <br >Alina Gaidies
+                <br >
+                <br ><b>Contact:</b>
+                <br >Phone: +49 176 81336606
+                <br >E–Mail: anny.startup@gmail.com
+                <br ><br ><br >
+            </div>
+            <div class="legal-content">
+                <h2>Liability for contents</h2>
+                <br >The contents of these pages were created with the greatest care. However, I can not guarantee the accuracy, completeness and timeliness of the content. As a service provider I am responsible according to § 7 Abs.1 TMG for own contents on these pages under the general laws. According to §§ 8 to 10 TMG I am not obligated to monitor transmitted or stored information or to investigate circumstances that indicate illegal activity. Obligations to remove or block the use of information under the general laws remain unaffected. However, liability in this regard is only possible from the point in time at which a concrete infringement of the law becomes known. Upon becoming aware of such infringements, this content will be removed immediately.
+                <br ><br ><br >
+                <h2>Liability for links</h2>
+                <br >This website contains links to external websites of third parties, on whose contents I have no influence. Therefore, I can not assume any liability for these external contents. The respective provider or operator of the pages is always responsible for the content of the linked pages. The linked pages were checked for possible legal violations at the time of linking. Illegal contents were not recognizable at the time of linking. However, a permanent control of the contents of the linked pages is not reasonable without concrete evidence of a violation of the law. If we become aware of any infringements, such links will be removed immediately.
+                <br ><br ><br >
+                <h2>Online dispute resolution</h2>
+                <br >The European Commission provides an online dispute resolution platform at <a href="https://ec.europa.eu/consumers">https://ec.europa.eu/consumers</a> that consumers can use to resolve a dispute and where more information on dispute resolution can be found.
+                <br ><br ><br >
+                <div class="copyright">
+                    <h2>Copyright Notice</h2>
+                    <br >© Copyright Yvonne Becker, Antonia Götz & Alina Gaidies Webdesign 2023. All rights reserved. Text, images, graphics, sound, animations and videos on this website are protected by copyright and other protective laws. The content of this website may not be copied, distributed, modified or made available to third parties.
+                    <br ><br ><br ><br ><br ><br >
+                </div>
+            </div>
         </div>
-      </div> 
     </div>
 </template>
-
-<script>
-definePageMeta({
-  layout: "simple",
-});
-</script>
-
-<script setup>
-import { onMounted } from 'vue'
-
-onMounted(() => {
-  window.scrollTo(0, 0)
-})
-</script>
