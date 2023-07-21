f439146d68b9f13dbebfd2f5d986037503d5c5a5
Yvonne Becker <73485093+yvnnbcker@users.noreply.github.com>
2023-07-21 23:40:19 +0200

    nur noch footer & header respo machen :D

--- components/footer.vue
+++ components/footer.vue
@@ -1,12 +1,13 @@
 <template>
         <footer>
+            <div id="footer-desktop">
             <div id="contact" class="footer-content">
                 <h1 class="slogan">ännything is<br> possible!</h1>
                 <h5 class="footer-text">Vision Quote: Sed mi ex, luctus sed sem vel, cursus pretium lorem. Phasellus placerat bibendum leo. Maecenas nec bibendum elit, at convallis sem.</h5>
                 <ButtonIcon
                 icon="smiley"
-                href="mail@mail.com"
-                >Schreib uns!</ButtonIcon>
+                href="mail@mail.com">
+                Schreib uns!</ButtonIcon>
             </div>
             <div class="footer-links">   
                 <div class="rechtliches">     
@@ -19,5 +20,27 @@
                     <NuxtLink class="btn-global" to="">mail</NuxtLink> 
                 </div>
             </div>
+        </div>
+        <div id="footer-mobile">
+            <div id="contact" class="footer-content">
+                <h1 class="slogan">ännything is<br> possible!</h1>
+                <h5 class="footer-text">Vision Quote: Sed mi ex, luctus sed sem vel, cursus pretium lorem. Phasellus placerat bibendum leo. Maecenas nec bibendum elit, at convallis sem.</h5>
+                <ButtonIcon
+                icon="smiley"
+                href="mail@mail.com">
+                Schreib uns!</ButtonIcon>
+            </div>
+            <div class="footer-links">   
+                <div class="rechtliches">     
+                    <NuxtLink class="btn-global" to="/datenschutz">Datenschutz</NuxtLink>  
+                    <NuxtLink class="btn-global" to="/impressum">Impressum</NuxtLink>  
+                </div>    
+                <div class="some">
+                    <NuxtLink class="btn-global" to="">instagram</NuxtLink>  
+                    <NuxtLink class="btn-global" to="">linkedin</NuxtLink>                 
+                    <NuxtLink class="btn-global" to="">mail</NuxtLink> 
+                </div>
+            </div>  
+        </div>
         </footer>
 </template>
--- components/hero.vue
+++ components/hero.vue
@@ -1,6 +1,16 @@
 <template>
-    <header id="start">
-        <div class="full-img">
+    <header>
+        <div class="hero-desktop">
+            <div class="hero-content">
+                <div class="hero-text">
+                    <h1 class="hero-title">Willkommen <br> bei änny!</h1>
+                    <h5>Deinem Lern-Spiel-Möbel, <br> das mitwächst.</h5>
+                </div>
+                <div class="ha-arrow"><img src="/scribble-arrow.svg"></div>
+                <div class="ha-dot"><img src="/scribble-dot.svg"></div>
+            </div>
+        </div>
+        <div class="hero-mobile">
             <div class="hero-content">
                 <div class="hero-text">
                     <h1 class="hero-title">Willkommen <br> bei änny!</h1>
