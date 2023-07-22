fa7928932c0972cc110418eac3a04a992d0f3a00
Yvonne Becker <73485093+yvnnbcker@users.noreply.github.com>
2023-07-22 10:19:05 +0200

    update gallery content

--- components/gallery.vue
+++ components/gallery.vue
@@ -5,14 +5,19 @@
         img="1-kind.jpg"
         alt="Kind umgeben von vielen Spielsachen"
         title="Überreizt?"
-        text="Die Nutzung von vielen verschiedenen Kinderspielzeugen kann zu Überreizung und kürzerer Aufmerksamkeitsspanne bei Kindern führen. Mit änny bekommen Kinder die Grundlage, um alles was sie wollen, selbstständig umzusetzten und spielerisch zu lernen. Dies fördert den Entdeckergeist und das Vorstellungsvermögen."/>
+        text="Die Nutzung von vielen verschiedenen Kinderspielzeugen kann zu Überreizung 
+        und kürzerer Aufmerksamkeitsspanne bei Kindern führen. Mit änny bekommen Kinder die Grundlage, 
+        um alles was sie wollen selbstständig umzusetzten und spielerisch zu lernen. 
+        Dies fördert den Entdeckergeist und das Vorstellungsvermögen."/>
         <TextImage
         img="1-eltern.jpg"
         alt="Vater am Schreibtisch verzweifelt, Kinder spielen im Hintergrund"
         title="Überfordert?"
         text="Eltern sind dafür verantwortlich, mit welchen Produkten ihr Kind Tag für Tag interagiert.
-Die Auswahl dieser ist einer riesen Herausforderung, da sich sowohl das Interesse, der Geschmack, als auch die Bedürfnisse und ergonomische Anforderungen stetig ändern. Demnach wenden sie viel Zeit, Geld und auch Nerven auf um die optimalen Produkte auszusuchen, zu kaufen und auch wieder loszuwerden.
-Mit änny kaufen kaufen Eltern nur einmal und können individuell ergänzen."
+        Die Auswahl dieser ist eine riesige Herausforderung, da sich sowohl das Interesse, der Geschmack, 
+        als auch die Bedürfnisse und ergonomische Anforderungen stetig ändern. Demnach wenden sie viel Zeit, 
+        Geld und auch Nerven auf um die optimalen Produkte auszusuchen, zu kaufen und auch wieder loszuwerden.
+        Mit änny kaufen Eltern nur einmal und können individuell ergänzen."
         order="img-first"/>
         <div class="scribble-fg scribble1">
             <img src="/scribble-arrow.svg">        
@@ -21,6 +26,8 @@ Mit änny kaufen kaufen Eltern nur einmal und können individuell ergänzen."
         img="1-muell.jpg"
         alt="Mülldeponie"
         title="Überlastet?"
-        text="Die kurze des Lebenszyklus und gleichzeitige Masse an Spielzeug und Kindermöbeln ist besorgniserregend. Mit änny wollen wir die Möglichkeit schaffen durch den Kauf von wenigen, lebenlangen Produkten unsere Erde zu entlasten. Wir schonen die Umwelt durch lange Nutzung, optinale AddOns und viele Funktionsbereiche."/>
+        text="Der kurze Lebenszyklus und gleichzeitig die Masse an Spielzeugen und Kindermöbeln ist besorgniserregend. 
+        Mit änny wollen wir die Möglichkeit schaffen durch den Kauf mit nur einem Produkt viele Funktionsbereiche abzudecken und somit Konsum zu reduzieren. 
+        Dadurch schonen wir schonen die Umwelt."/>
     </div>
 </template>
--- components/hero.vue
+++ components/hero.vue
@@ -15,7 +15,7 @@
             <div class="hero-content">
                 <div class="hero-text">
                     <h1 class="hero-title">Willkommen <br> bei änny!</h1>
-                    <h5>Deinem Lern-Spiel-Möbel, <br> das mitwächst.</h5>
+                    <h5>Deinem Lern-Spiel-Möbel, das mitwächst.</h5>
                     <p>Wir sind ein Startup zum mitlernen und ausprobieren! Aktuell sind wir noch in der Entwicklungsphase und zeigen unsere Produkte und konkreten Umsetzungen nicht. Aber hier kannst du dich umschauen, unseren Weg verfolgen und somit Teil des Ganzen werden.</p>
                 </div>
                 <div class="ha-arrow"><img src="/scribble-arrow.svg"></div>
